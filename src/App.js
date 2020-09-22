dbd0562f02f95e2bdcdd6aca634d166646bf77ed
Avnish <avnish.jayaswal@gmail.com>
2020-09-22 18:26:18 +0530

    React hook form functional component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useForm } from "react-hook-form";
+import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css' ;
 
-function App() {
+export default function App() {
+  const { register, handleSubmit, errors } = useForm();
+  const onSubmit = (data) => console.log(data);
+  console.log(errors);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <section class="login-block">
+    <div className="container-fluid auth-box card">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="row">
+          <div className="md-col-6 offset-4 mt-3">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="First name"
+              name="First name"
+              ref={register({ required: true, maxLength: 80 })}
+            />
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="md-col-6 offset-4 mt-3">
+            <input
+              className="form-control"
+              type="text"
+              placeholder="Last name"
+              name="Last name"
+              ref={register({ required: true, maxLength: 100 })}
+            />
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="md-col-6 offset-4 mt-3">
+            <input
+              className="form-control"
+              type="text"
+              placeholder="Email"
+              name="Email"
+              ref={register({ required: true, pattern: /^\S+@\S+$/i })}
+            />
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="md-col-6 offset-4 mt-3 mb-3">
+            <input type="submit" />
+          </div>
+        </div>
+      </form>
     </div>
+    </section>
   );
 }
-
-export default App;
